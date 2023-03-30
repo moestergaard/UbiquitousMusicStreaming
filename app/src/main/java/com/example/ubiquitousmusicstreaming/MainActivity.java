0423353225991ab0cd2543ac84af50099e4cc458
Martin Oestergaard <martin.malmkjaer.oestergaard@gmail.com>
2023-03-30 13:27:20 +0200

    Working with spotify playing music in the background.

--- app/src/main/java/com/example/ubiquitousmusicstreaming/MainActivity.java
+++ app/src/main/java/com/example/ubiquitousmusicstreaming/MainActivity.java
@@ -7,6 +7,7 @@ import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.Manifest;
+import android.util.Log;
 
 import com.example.ubiquitousmusicstreaming.ui.configuration.ConfigurationFragment;
 import com.example.ubiquitousmusicstreaming.ui.location.LocationFragment;
@@ -24,6 +25,15 @@ import com.example.ubiquitousmusicstreaming.databinding.ActivityMainBinding;
 
 import java.util.List;
 
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
@@ -36,6 +46,11 @@ public class MainActivity extends AppCompatActivity {
     private static DataManagement dm;
     private static String predictedRoom, previousLocation;
     private static String[] locations = new String[]{"Kontor", "Stue", "Køkken"};
+
+    private static final String CLIENT_ID = "6e101d8a913048819b5af5e6ee372b59";
+    private static final String REDIRECT_URI = "ubiquitousmusicstreaming-login://callback";
+    //private static final String REDIRECT_URI = "http://com.yourdomain.yourapp/callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -87,16 +102,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         // We will start writing our code here.
+        // Set the connection parameters
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+
+                    @Override
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("MainActivity", "Connected! Yay!");
+
+                        // Now you can start interacting with App Remote
+                        connected();
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.e("MainActivity", throwable.getMessage(), throwable);
+
+                        // Something went wrong when attempting to connect! Handle errors here
+                    }
+                });
     }
 
 
     private void connected() {
         // Then we will write some more code here.
+        // Play a playlist
+        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX7K31D69s4M1");
+        // Subscribe to PlayerState
+        mSpotifyAppRemote.getPlayerApi()
+                .subscribeToPlayerState()
+                .setEventCallback(playerState -> {
+                    final Track track = playerState.track;
+                    if (track != null) {
+                        Log.d("MainActivity", track.name + " by " + track.artist.name);
+                    }
+                });
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
         // Aaand we will finish off here.
     }
 
